ddc38ee035b901da563388027197b6c6a9c42507
Andrey <99925434+X-Newbie@users.noreply.github.com>
2022-08-16 11:12:39 +0300

    -
    -

--- assets/product-card.739d489e.js
+++
@@ -1,1 +0,0 @@
-import"./input.655a1f2d.js";/* empty css               */import"./shaking-buttons.a25734bd.js";import"./product-card.fe522015.js";import"./vendor.7779e7f7.js";import"./modal.1489b151.js";
